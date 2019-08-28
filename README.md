6e820ecbf416bde820a58a46a6be69c753c213c9
Aaron Lichtman <aaronlichtman@gmail.com>
2019-08-28 07:49:22 +0200

    Minimal version complete
    Fix #1, Fix #5

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # zsh-startify
 
-This is a terminal splash screen for zsh. After starting a terminal session, it will load `tmux`, show you sessions you can attach to. It will also give you the option to create a new named sesion, as well as launch a non-`tmux` session.
+**TODO: Insert demo.**
+
+This is a start screen for zsh. After starting a terminal session, it will load `tmux` and show you sessions you can attach to. It will also give you the option to create a new named sesion, as well as launch a regular `zsh` session.
+
+## Inspiration
+
+I've used [vim-startify](https://github.com/mhinz/vim-startify) pretty much every day for the past year. I figured it was time for `zsh` and `tmux` to have a similar tool.
--- zsh_startify.py
+++ zsh_startify.py
@@ -1,4 +1,10 @@
+# Standard Library Imports
+import os
+from time import sleep
+
+# 3rd Party Library Imports
 import libtmux
+from halo import Halo
 from pyfiglet import Figlet
 from colorama import Fore, Style
 from PyInquirer import prompt, style_from_dict, Token, \
@@ -9,8 +15,20 @@ from PyInquirer import prompt, style_from_dict, Token, \
 # Prettify #
 ############
 
+def print_blue(text):
+    print(Fore.BLUE + Style.BRIGHT + text + Style.RESET_ALL)
+
+
+def print_red(text):
+    print(Fore.RED + Style.BRIGHT + text + Style.RESET_ALL)
+
+
+def print_yellow(text):
+    print(Fore.YELLOW + Style.BRIGHT + text + Style.RESET_ALL)
+
+
 def print_green(text):
-    print(Fore.GREEN + text + Style.RESET_ALL)
+    print(Fore.GREEN + Style.BRIGHT + text + Style.RESET_ALL)
 
 
 def splash(text, font):
@@ -18,8 +36,16 @@ def splash(text, font):
     # TODO: Add splash screen config option in .zshrc
     # TODO: Add pretty colors
     fig = Figlet(font=font)
-    text = fig.renderText(text)
-    print_green(text.replace("\n", "\n\t"))
+    text = fig.renderText(text).replace("\n", "\n\t")
+    print_green(text)
+
+
+def sleep_with_spinner(secs):
+    """Sleep with a nice spinner and then print a success messsage."""
+    spinner = Halo(text='Loading', spinner='dots')
+    spinner.start()
+    sleep(secs)
+    spinner.succeed("Success.")
 
 
 ########
@@ -27,17 +53,55 @@ def splash(text, font):
 ########
 
 def get_tmux_session_names(server) -> list:
+    """Returns all current tmux session names"""
     return [session.name for session in server.list_sessions()]
 
 
-def attach_to_session(server, name):
+def get_tmux_session(server, name) -> libtmux.Session:
+    """Looks up a tmux session by name and returns it if it exists. Returns
+    None if the session doesn't exist.
+    """
     session = server.find_where({"session_name": name})
+    if session is None:
+        raise ValueError(f"Error: No session of name \'{name}\' exists.")
+    return session
+
+
+def attach_to_session(server, name):
+    session = get_tmux_session(server, name)
     session.attach_session()
 
 
 def create_new_named_session(server, name):
     session = server.new_session(name)
     session.attach_session()
+
+
+def get_tmux_server() -> libtmux.Server:
+    """
+    Returns tmux server if it's running, and starts one if it's not.
+    """
+    server = libtmux.Server()
+    try:
+        server.list_sessions()
+    except libtmux.exc.LibTmuxException:
+        print_blue("Starting tmux server...")
+        # Apparently, there is no way to start a server without any sessions
+        # (https://github.com/tmux/tmux/issues/182). So, we're going to spawn a
+        # new server, create a dummy session and then kill it later. I'm sorry.
+        hack_session = "zsh-startify-tmux-server-hack"
+        start_server_hack = f"tmux has-session -t {hack_session} || \
+                              tmux new-session -d -s {hack_session}"
+        os.system(start_server_hack)
+        # Sleep to give tmux some time to restore sessions if tmux-resurrect or
+        # tmux-continuum is used
+        # TODO: Make this sleep value configurable in .zshrc
+        sleep_with_spinner(5)
+        server = libtmux.Server()
+        # Kill the hacky tmux session we just started.
+        get_tmux_session(server, hack_session).kill_session()
+
+    return server
 
 
 #############
@@ -55,6 +119,8 @@ ACTIONS = {
     "attach-tmux-session": "Attach to existing tmux session",
 }
 
+# TODO: Finalize default colors
+# TODO: Make colors configurable. Maybe add color themes?
 SELECTOR_STYLE = style_from_dict({
         Token.QuestionMARK: '#dcbf85 bold',
         Token.Question: '#1776A5 bold',
@@ -71,8 +137,9 @@ def prompt_for_action(tmux_sessions) -> str:
     session to attach to, the option to create a new session, or create a
     non-tmux-session session.
     """
+    launch_zsh = MARK["non-tmux-session"] + ACTIONS["zsh"]
     choices = [
-        MARK["non-tmux-session"] + ACTIONS["zsh"],
+        launch_zsh,
         MARK["non-tmux-session"] + ACTIONS["new-tmux-session"],
         {
             'name': ACTIONS["attach-tmux-session"],
@@ -91,12 +158,16 @@ def prompt_for_action(tmux_sessions) -> str:
         },
     ]
 
-    # TODO: Finalize default colors
-    # TODO: Make colors configurable. Maybe add color themes?
-    answers = prompt(questions, style=SELECTOR_STYLE)
+    try:
+        answers = prompt(questions, style=SELECTOR_STYLE)
+    # Handle CTRL+D
+    except EOFError:
+        return launch_zsh
 
-    print(answers["action"])
-    return answers["action"]
+    if answers == {}:
+        return launch_zsh
+    else:
+        return answers["action"]
 
 
 class TmuxSessionValidator(Validator):
@@ -137,13 +208,11 @@ def prompt_for_session_name() -> str:
 
 def action_handler(server, action):
     # Attach to session
-    print("ACTION:", action)
     if action.startswith(MARK["tmux-session"]):
         attach_to_session(server, action.strip())
     # Create new session
     elif ACTIONS["new-tmux-session"] in action:
         session_name = prompt_for_session_name()
-        print("Session name:", session_name)
         create_new_named_session(server, session_name)
     # Launch zsh
     else:
@@ -152,10 +221,8 @@ def action_handler(server, action):
 
 def main():
     splash("zsh", "univers")
-    # TODO: What happens if no server is running?
-    server = libtmux.Server()
+    server = get_tmux_server()
     tmux_sessions = get_tmux_session_names(server)
-    #  print("TMUX Sessions:", tmux_sessions)
     action = prompt_for_action(tmux_sessions)
     action_handler(server, action)
 
